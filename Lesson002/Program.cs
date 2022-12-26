b02dd6b5a8b79c400e59a0c3b1d57e61c394df35
Evgenii <89522778813@mail.ru>
2022-12-26 17:30:05 +0300

    Add Lesson 002

---
+++ Lesson002/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+if (a>b) 
+{     
+    WriteLine("Первое число больше второго"); 
+}
+else if (a<b) 
+{   
+    WriteLine("Второе число больше первого");
+}
